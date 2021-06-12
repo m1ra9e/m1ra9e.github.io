d76f51d1b51f42c63e4e1ea003ecf0ba72c88b46
l.shamsutdinov <l.shamsutdinov@mail.ru>
2021-06-12 16:36:02 +0300

    styles for mobile devices are separated from styles for pc

--- js/css.js
+++ js/css.js
@@ -1,9 +1,9 @@
 // the timestamp is added to prevent css file from being cached
 // example of result: <link href="css/style.css?t=1621090880373" rel="stylesheet" type="text/css">
 
-function addStyle(fileName, timestamp) {
+function addStyle(fileName, timestamp, cssDeviceTypeFolder) {
     let style = document.createElement('link');
-    style.setAttribute('href', "css/" + fileName + "?t=" + timestamp);
+    style.setAttribute('href', "css/" + cssDeviceTypeFolder + "/" + fileName + "?t=" + timestamp);
     style.setAttribute('rel', 'stylesheet');
     style.setAttribute('type', 'text/css');
     document.head.appendChild(style);
@@ -15,13 +15,11 @@ function isMobileDevice() {
 }
 
 let timestamp = new Date().getTime();
+let cssDeviceTypeFolder = isMobileDevice() ? "mobile" : "pc";
 
-addStyle("common.css", timestamp);
-addStyle("disguise.css", timestamp);
-addStyle("creeping_line.css", timestamp);
-addStyle("info.css", timestamp);
-addStyle("tabs.css", timestamp);
-
-if (!isMobileDevice()) {
-    addStyle("side_elements.css", timestamp);
-}
+addStyle("common.css", timestamp, cssDeviceTypeFolder);
+addStyle("disguise.css", timestamp, cssDeviceTypeFolder);
+addStyle("creeping_line.css", timestamp, cssDeviceTypeFolder);
+addStyle("info.css", timestamp, cssDeviceTypeFolder);
+addStyle("tabs.css", timestamp, cssDeviceTypeFolder);
+addStyle("side_elements.css", timestamp, cssDeviceTypeFolder);
